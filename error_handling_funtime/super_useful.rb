c5cbce1ad551985e680afb8771a8cda1ab3f8d7e
tjohnny93 <tjohnny93@berkeley.edu>
2019-05-20 14:14:37 -0700

    Fix super useful

--- error_handling_funtime/super_useful.rb
+++ error_handling_funtime/super_useful.rb
@@ -1,30 +1,55 @@
 # PHASE 2
 def convert_to_int(str)
   Integer(str)
+  rescue ArgumentError
+  raise "Unable to convert"
+  return nil
+
 end
 
 # PHASE 3
 FRUITS = ["apple", "banana", "orange"]
 
 def reaction(maybe_fruit)
+  
   if FRUITS.include? maybe_fruit
     puts "OMG, thanks so much for the #{maybe_fruit}!"
-  else 
+  else
+    puts "I don't want that"
     raise StandardError 
-  end 
+    
+  end
 end
 
 def feed_me_a_fruit
-  puts "Hello, I am a friendly monster. :)"
+    puts "Hello, I am a friendly monster. :)"
+  begin
+    puts "Feed me a fruit! (Enter the name of a fruit:)"
+    maybe_fruit = gets.chomp
+    reaction(maybe_fruit)
+  rescue
+    puts "I'm angry now, give me coffee!"
+    if gets.chomp == "coffee"
 
-  puts "Feed me a fruit! (Enter the name of a fruit:)"
-  maybe_fruit = gets.chomp
-  reaction(maybe_fruit) 
+      retry
+    end
+  end
 end  
 
 # PHASE 4
 class BestFriend
   def initialize(name, yrs_known, fav_pastime)
+    if yrs_known < 5
+      raise "That's not enough years to be bestie"
+    end
+    if name.length <= 0
+      raise "That is not a name!"
+    end
+    if fav_pastime.length <= 0
+      raise "How dare you!?"
+    end
+
+
     @name = name
     @yrs_known = yrs_known
     @fav_pastime = fav_pastime
--- error_handling_funtime/user_script.rb
+++ error_handling_funtime/user_script.rb
@@ -1,10 +1,10 @@
 require_relative 'super_useful'
 
-puts "'five' == #{convert_to_int('five')}"
+# puts "'five' == #{convert_to_int('five')}"
 
-feed_me_a_fruit
+# feed_me_a_fruit
 
-sam = BestFriend.new('', 1, '')
+sam = BestFriend.new('Bob', 7, 'Six flags')
 
 sam.talk_about_friendship
 sam.do_friendstuff
